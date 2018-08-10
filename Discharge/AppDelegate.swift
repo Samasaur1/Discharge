1a550e148216003560d06841028cf57345befff2
Sam <samgauck@icloud.com>
2018-08-10 18:50:57 -0400

    Initial commit

---
+++ Discharge/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Discharge
+//
+//  Created by Sam Gauck on 8/10/18.
+//  Copyright © 2018 Sam Gauck. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
